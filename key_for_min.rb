e726195f98fc748f5292087e1a406abe648837eb
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-19 19:00:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,5 @@ def key_for_min_value(name_hash)
   values_array = name_hash.collect{|key, value| value}.sort
     name_hash.each{|key, value| return key if value == values_array[0]}
     nil
-  end 	end 
+  end 	end
 end
